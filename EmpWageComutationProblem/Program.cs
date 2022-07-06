bcf99c76017f04cae65038169fbfc89cc7fd2ef8
Satish patil <Satish.patil7017@gmail.com>
2022-07-06 19:35:06 +0530

    First commit

---
+++ EmpWageComutationProblem/Program.cs
@@ -0,0 +1,33 @@
+﻿//Console.WriteLine("Welcome To Employee Wage Computation Problem");
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //UC1
+            EmpPresentAbsent prg = new EmpPresentAbsent();
+            prg.CheckEmpPresentAbsent();
+
+            //UC2
+            //DailyEmpWage prg = new DailyEmpWage();
+            //prg.CheckDailyEmpWg();
+
+            //UC3
+            //PartTimeEmpWage prg = new PartTimeEmpWage();
+            //prg.CheckPartTimeEmpWage();
+
+            //UC4
+            //SwitchCaseProb prg = new SwitchCaseProb();
+            //prg.CalculateEmpWage();
+
+            //UC5
+            //CalculatingWagesForMonth prg = new CalculatingWagesForMonth();
+            //prg.CalculateEmpWage();
+
+            //UC6
+            //EmployeeWage prg = new EmployeeWage();
+            //prg.CalculateEmpWage();
+        }
+    }
+}
